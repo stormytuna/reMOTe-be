f0a2d433b6d70c72a101b6536dadbb460d489331
stormytuna <stormytuna@outlook.com>
2023-01-23 12:53:29 +0000

    Add custom error handling, 404s and 500s

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const {
+  handleCustomErrors,
+  handle404s,
+  handle500s,
+} = require("./controllers/controllers.errors");
 const { getTechnicians } = require("./controllers/controllers.technicians");
 
 const app = express();
@@ -6,5 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/technicians", getTechnicians);
+
+app.use(handleCustomErrors);
+app.use("/*", handle404s);
+app.use(handle500s);
 
 module.exports = app;
---
+++ controllers/controllers.errors.js
@@ -0,0 +1,16 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(error);
+  }
+};
+
+exports.handle404s = (req, res, next) => {
+  res.status(404).send({ msg: "Content not found" });
+};
+
+exports.handle500s = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal server error" });
+};
