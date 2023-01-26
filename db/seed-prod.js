d881271f204a6dfe2b60d8f1c1cd35855e1e7180
stormytuna <stormytuna@outlook.com>
2023-01-26 15:46:30 +0000

    Remove console logs and fix seedProd not running properly

--- db/seed-prod.js
+++ db/seed-prod.js
@@ -6,14 +6,11 @@ const prodData = require("./data/prod.json");
 exports.seedProd = async () => {
   try {
     connect();
-    console.log("Seeding prod...");
     await User.deleteMany({});
-    console.log("Deleted current data!");
     await User.insertMany(prodData);
-    console.log(prodData);
-    console.log("Added data from prodData!");
     disconnect();
   } catch (e) {
     console.error(e.message);
   }
 };
+seedProd();
