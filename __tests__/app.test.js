baddb1e758e9cbdccc10f490686270ebaefa3828
stormytuna <82264356+stormytuna@users.noreply.github.com>
2023-01-27 10:30:01 +0000

    Revert "Betterer schema"

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,12 +7,12 @@ const { disconnect } = require("../db/end-connection");
 const { seedTest } = require("../db/seed-test");
 const mongoose = require("mongoose");
 
-beforeAll(async () => {
-  return await connect();
+beforeAll(() => {
+  return connect();
 });
 
-beforeEach(async () => {
-  return await seedTest();
+beforeEach(() => {
+  return seedTest(userData);
 });
 
 afterAll(() => {
@@ -66,7 +66,6 @@ describe("POST /api/technicians", () => {
           { name: "Servicing and MOT", price: 45 },
           { name: "clutch repair", price: 100 },
         ],
-        company: "Ahmed's Repairs",
       },
       avatarUrl: "https://i.imgur.com/pN04qjy.jpg",
     };
